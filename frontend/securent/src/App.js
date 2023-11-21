8c3ee6cd92906ab0220c35447d19debdbaa51449
MustafaKaanKoc <m.kaankoc8@gmail.com>
2023-11-21 15:05:52 +0300

    pages folder

--- frontend/securent/src/App.js
+++ frontend/securent/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-
-import HomePage from './HomePage.js';
+import ProfilePage from './pages/ProfilePage';
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <ProfilePage/>
     </div>
   );
 }
