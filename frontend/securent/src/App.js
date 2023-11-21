23dab168c28b54b73d6452899ba7b53b4415523a
MustafaKaanKoc <m.kaankoc8@gmail.com>
2023-11-21 16:47:13 +0300

    update

--- frontend/securent/src/App.js
+++ frontend/securent/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Button from 'react-bootstrap/esm/Button';
 import ProfilePage from './pages/ProfilePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Login from './pages/Login';
 
 
 function App() {
   return (
-    <div className="App">
-      <ProfilePage/>
-      <Button>asf</Button>
+    <div className="icon-grid">
+      <Login/>
    </div>
   );
 }
