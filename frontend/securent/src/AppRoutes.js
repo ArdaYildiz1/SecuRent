c1de006844b4a7659c95fb5ef405487563a4f245
Yunus Eren Türkeri <yerent2000@gmail.com>
2023-11-21 14:43:12 +0300

    appjs

---
+++ frontend/securent/src/AppRoutes.js
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+
+const AppRoutes = [
+    {
+        index: true,
+        path: '/home',
+        element: <Home/>
+    },
+    // {
+    //     path: '/fetch-data',
+    //     element: <FetchData/>
+    // },
+
+
+];
+
+export default AppRoutes;
