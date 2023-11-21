de79af507f45cac02ca7defd367ff53158815725
MustafaKaanKoc <m.kaankoc8@gmail.com>
2023-11-22 00:00:12 +0300

    select agent page is done

--- frontend/securent/src/App.js
+++ frontend/securent/src/App.js
@@ -3,12 +3,11 @@ import Button from 'react-bootstrap/esm/Button';
 import ProfilePage from './pages/ProfilePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
-
-
+import SelectAgent from './pages/SelectAgent';
 function App() {
   return (
-    <div className="icon-grid">
-      <Login/>
+    <div className="">
+      <SelectAgent/>
    </div>
   );
 }
