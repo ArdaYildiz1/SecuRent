97673cb8f5ded575666f23058a42eb0c6a11035b
Yunus Eren Türkeri <yerent2000@gmail.com>
2023-11-21 15:26:57 +0300

    app css ekledim

--- frontend/securent/src/App.js
+++ frontend/securent/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from './pages/Login';
 
 import ProfilePage from './pages/ProfilePage';
 function App() {
   return (
-    <div className="App">
-      <ProfilePage/>
+    <div className='icon-grid'>
+      <Login/>
     </div>
   );
 }
