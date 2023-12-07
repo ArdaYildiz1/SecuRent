6ed5c634f9c82867af41505af576fbfef83cc734
MustafaKaanKoc <m.kaankoc8@gmail.com>
2023-12-07 18:24:30 +0300

    rest of the UI's

--- frontend/securent/src/AppRoutes.js
+++ frontend/securent/src/AppRoutes.js
@@ -3,7 +3,25 @@ import HouseDetails from "./pages/HouseDetails";
 import Login from "./pages/Login";
 import TenantProfile from "./pages/TenantProfile";
 import SelectAgent from "./pages/SelectAgent";
-
+import LandlordProfile from "./pages/LandlordProfile";
+import RealEstateAgentProfile from "./pages/RealEstateAgentProfile";
+import StateOfficialProfile from "./pages/StateOfficialProfile";
+import LandlordNotification from "./pages/LandlordNotificaiton";
+import RealEstateAgentNotification from "./pages/RealEstateAgentNotificaiton";
+import StateOfficialNotification from "./pages/StateOfficialNotification";
+import HandledComplaints from "./pages/HandledComplaints";
+import PendingComplaints from "./pages/PendingComplaints";
+import Contracts from "./pages/Contracts";
+import ApprovedContracts from "./pages/ApprovedContracts";
+import LandlordPastRequest from "./pages/LandlordPastRequest";
+import LandlordCurrentRequest from "./pages/LandlordCurrentRequest";
+import RealEstateAgentPastRequest from "./pages/RealEstateAgentPastRequest";
+import RealEstateAgentCurrentRequest from "./pages/RealEstateAgentCurrentRequest";
+import SeeTenantProfile from "./pages/SeeTenantProfile";
+import SeeLandlordProfile from "./pages/SeeLandlordProfile";
+import SeeRealEstateAgentProfile from "./pages/SeeRealEstateAgentProfile";
+import SeeTenantAndLandlord from "./pages/SeeTenantAndLandlord";
+import SeeTenantAndRealEstateAgent from "./pages/SeeTenantAndRealEstateAgent";
 
 const AppRoutes = [
     {
@@ -26,9 +44,83 @@ const AppRoutes = [
     {
         path: '/tenantProfile',
         element: <TenantProfile/>
+    }, 
+    {
+        path: '/landlordProfile',
+        element: <LandlordProfile/>
+    },   
+    {
+        path: '/realEstateAgentProfile',
+        element: <RealEstateAgentProfile/>
+    },     
+    {
+        path: '/stateOfficialProfile',
+        element: <StateOfficialProfile/>
+    },
+    {
+        path: '/landlordNotification',
+        element: <LandlordNotification/>
     },    
-
-
+    {
+        path: '/realEstateAgentNotification',
+        element: <RealEstateAgentNotification/>
+    }, 
+    {
+        path: '/stateOfficialNotification',
+        element: <StateOfficialNotification/>
+    }, 
+    {
+        path: '/handledComplaints',
+        element: <HandledComplaints/>
+    },
+    {
+        path: '/pendingComplaints',
+        element: <PendingComplaints/>
+    },  
+    {
+        path: '/contracts',
+        element: <Contracts/>
+    },            
+    {
+        path: '/approvedContracts',
+        element: <ApprovedContracts/>
+    },
+    {
+        path: '/landlordPastRequest',
+        element: <LandlordPastRequest/>
+    },
+    {
+        path: '/landlordCurrentRequest',
+        element: <LandlordCurrentRequest/>
+    },
+    {
+        path: '/realEstateAgentPastRequest',
+        element: <RealEstateAgentPastRequest/>
+    },
+    {
+        path: '/realEstateAgentCurrentRequest',
+        element: <RealEstateAgentCurrentRequest/>
+    },
+    {
+        path: '/seeTenantProfile',
+        element: <SeeTenantProfile/>
+    },
+    {
+        path: '/seeLandlordProfile',
+        element: <SeeLandlordProfile/>
+    },
+    {
+        path: '/seeRealEstateAgentProfile',
+        element: <SeeRealEstateAgentProfile/>
+    },
+    {
+        path: '/seeTenantAndLandlord',
+        element: <SeeTenantAndLandlord/>
+    },
+    {
+        path: '/seeTenantAndRealEstateAgent',
+        element: <SeeTenantAndRealEstateAgent/>
+    },
 ];
 
 export default AppRoutes;
---
+++ frontend/securent/src/pages/RealEstateAgentPastRequest.jsx
@@ -0,0 +1,105 @@
+import Button from 'react-bootstrap/esm/Button';
+import React from 'react';
+import pp from './defaultPP.jpg';
+import TopNavBar from './TopNavBar';
+import { Card, Col, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function RealEstateAgentPastRequest() {
+
+
+    const dynamicData = ['Ahmet Kalem', 'Fatih Kor', 'Ali Tarık', 'Kasım Kale', 'Fatih Kor', 'Ali Tarık', 'Ahmet Kalem', 'Fatih Kor', 'Ali Tarık', 'Mehmet Ersoy', 'Fatih Kor', 'Ali Tarık', 'Mehmet Ersoy', 'Fatih Kor', 'Ali Tarık', 'Mehmet Ersoy'];
+    const landlord = ['Ali Kemal Yazıcı', 'Fatih Terim', 'Aslı Kavun' ,'Melike Bodur'];
+
+    const DynamicList = ({ data, landlord }) => {
+        return (
+            <>
+
+                <div className="row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8">
+                        <Row xs={1} md={3} className="g-4">
+                            <>
+                                {Array.from({ length: 4 }).map((_, idx) => (
+                                    <Col key={idx}>
+                                        {/* burayı belki bir profile yönlendiririz   to={`/houseDetails`}*/}
+                                        <Link style={{ textDecoration: 'none' }}>
+                                            <Card
+                                                className="clickable-card"
+                                                style={{ backgroundColor: '#f1f2ed', borderRadius: '2rem' }}
+                                            >
+                                                <br />
+                                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: 30 }}>
+                                                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                                        <h4>Tenant</h4>
+                                                        <Card.Img
+                                                            style={{ borderRadius: '1rem', width: '120px', height: '120px' }}
+                                                            variant="top"
+                                                            src={pp}
+                                                        />
+                                                    </div>
+                                                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                                        <h4 >Landlord</h4>
+
+                                                        <Card.Img
+
+                                                            style={{ borderRadius: '1rem', width: '120px', height: '120px', marginLeft: 10 }}
+                                                            variant="top"
+                                                            src={pp}
+                                                        />
+                                                    </div>
+
+                                                </div>
+
+                                                <Card.Body>
+                                                    <Card.Title style={{ color: "black" }}>{dynamicData[idx]}</Card.Title>
+                                                    <Card.Text style={{ color: "black" }}>
+                                                        {dynamicData[idx]} requested to see house {idx} owned by {landlord[idx]} .
+                                                    </Card.Text>
+
+                                                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+
+                                                        <Button style={{ width: '150px', height: '50px' }} className="btn-teal"
+                                                            type="submit">
+                                                            See the Profiles
+                                                        </Button>
+                                                    </div>
+
+                                                    <br />
+                                                </Card.Body>
+                                            </Card>
+                                        </Link>
+                                    </Col>
+                                ))}
+                            </>
+
+                        </Row>
+                    </div>
+                    <div className="col-md-2"></div>
+                </div>
+            </>
+        );
+    };
+
+    return (
+        <>
+            <TopNavBar />
+
+            <div style={{ width: '100%', height: '100vh', overflowY: 'auto' }}>
+
+                <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '2%', paddingBottom: '2%' }}>
+                    <h1 style={{ color: "white" }}>
+                        Past Requests
+                    </h1>
+                </div>
+
+                <div style={{ width: '100%', height: '100vh', }}>
+                    <DynamicList data={dynamicData} landlord={landlord} />
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default RealEstateAgentPastRequest;
