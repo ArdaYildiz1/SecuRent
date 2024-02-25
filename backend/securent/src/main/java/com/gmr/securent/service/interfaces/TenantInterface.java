1c3e7d0f7a42f3ad4968d17fb0fd1e9a3a82ac07
Arda YILDIZ <ardayildiz1074@gmail.com>
2024-02-25 12:49:36 +0300

    feat: continue tenant class

---
+++ backend/securent/src/main/java/com/gmr/securent/service/interfaces/TenantInterface.java
@@ -0,0 +1,23 @@
+package com.gmr.securent.service.interfaces;
+
+import com.gmr.securent.model.RentRequest;
+
+
+public interface TenantInterface {
+    
+    public void payDeposit(int amount);
+
+    public void sendRentingRequestToLandlord(RentRequest rentRequest);
+
+    public void cancelRentingRequestToLandlord(RentRequest rentRequest);
+
+    public void requestRentalService() ;
+
+    public void requestRentPriceToBeRecalculated();
+
+    public void acceptExtension(boolean renewContract);
+
+    public void reportAd();
+
+    public void rateRealEstateAgent(int agentId);
+}
