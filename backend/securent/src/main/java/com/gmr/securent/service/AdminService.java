0a9c0ecb8dd63678132694c327b29fddd3fc2506
Cengizhan Terzioğlu <cengizhan@ug.bilkent.edu.tr>
2024-02-28 00:48:46 +0300

    Forgot to press Ctrl+S lmao

---
+++ backend/securent/src/main/java/com/gmr/securent/service/AdminService.java
@@ -0,0 +1,28 @@
+package com.gmr.securent.service;
+
+import org.springframework.stereotype.Service;
+
+import com.gmr.securent.service.interfaces.AdminInterface;
+
+@Service
+public class AdminService implements AdminInterface {
+
+    @Override
+    public void listUsers() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void openGovernmentAccount() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void updateUsers() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
