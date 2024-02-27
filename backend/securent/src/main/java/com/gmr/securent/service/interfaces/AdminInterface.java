e387a8b26fbb3962f7014857527391c71de822a1
Cengizhan Terzioğlu <cengizhan@ug.bilkent.edu.tr>
2024-02-28 00:48:03 +0300

    Admin service created

---
+++ backend/securent/src/main/java/com/gmr/securent/service/interfaces/AdminInterface.java
@@ -0,0 +1,7 @@
+package com.gmr.securent.service.interfaces;
+
+public interface AdminInterface {
+    public void openGovernmentAccount();
+    public void listUsers();
+    public void updateUsers();
+}
