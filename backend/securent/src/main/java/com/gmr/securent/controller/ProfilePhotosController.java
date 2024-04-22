0a96d3f1ac413a159ca7c28561945ce367663d31
Arda YILDIZ <ardayildiz1074@gmail.com>
2024-04-22 22:39:13 +0300

    feat: add photo service for profile and house, add missing house properties

--- backend/securent/src/main/java/com/gmr/securent/controller/ProfilePhotosController.java
+++ backend/securent/src/main/java/com/gmr/securent/controller/ProfilePhotosController.java
@@ -1,7 +1,7 @@
 package com.gmr.securent.controller;
 
-import com.gmr.securent.entity.Contract;
-import com.gmr.securent.repository.ContractRepository;
+import com.gmr.securent.entity.ProfilePhotos;
+import com.gmr.securent.repository.ProfilePhotosRepository;
 import com.gmr.securent.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,26 +15,26 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/contracts")
-public class ContractController {
+@RequestMapping("/profile-photos")
+public class ProfilePhotosController {
     @Autowired
     private FileUploadService fileUploadService;
 
     @Autowired
-    private ContractRepository contractRepository;
+    private ProfilePhotosRepository profilePhotosRepository;
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
-                                             @RequestParam("landlordId") Integer landlordId) {
+                                             @RequestParam("userId") Integer userId) {
         try {
             String filePath = fileUploadService.uploadFile(file);
-            Contract contract = new Contract();
-            contract.setLandlordID(landlordId); // Set landlordId
-            contract.setFilePath(filePath);
-            contractRepository.save(contract);
-            return ResponseEntity.ok("Contract file uploaded successfully");
+            ProfilePhotos profilePhotos = new ProfilePhotos();
+            profilePhotos.setUserId(userId);
+            profilePhotos.setFilePath(filePath);
+            profilePhotosRepository.save(profilePhotos);
+            return ResponseEntity.ok("Profile photo uploaded successfully");
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload contract file");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload profile photo");
         }
     }
 }
---
+++ backend/securent/src/main/java/com/gmr/securent/repository/ProfilePhotosRepository.java
@@ -0,0 +1,7 @@
+package com.gmr.securent.repository;
+
+import com.gmr.securent.entity.ProfilePhotos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilePhotosRepository extends JpaRepository<ProfilePhotos, Long> {
+}
